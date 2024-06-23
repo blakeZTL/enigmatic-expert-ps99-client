07b60a68cf45871026e291ad20d0286bfc69fd53
Blake Bradford <james.b.bradford@faa.gov>
2024-06-23 10:34:50 -0400

    remove unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import ClanTotals from '$lib/components/ClanTotals.svelte';
 	import ClanDetails from '$lib/components/ClanDetails.svelte';
 	import { selectedClan, loadingData } from '$lib/stores';
-	import TablePlaceholder from '$lib/components/TablePlaceholder.svelte';
 </script>
 
 {#if $selectedClan !== ''}
